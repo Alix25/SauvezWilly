e37090b712716b46af6ddd95d4f1a5948d27938d
Alix <alix.stirling@efrei.net>
2021-06-25 11:39:14 +0200

    Ajout permission caméra

--- app/src/main/java/com/example/sauvezwilly/Fragment1.java
+++ app/src/main/java/com/example/sauvezwilly/Fragment1.java
@@ -1,7 +1,10 @@
 package com.example.sauvezwilly;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.AssetFileDescriptor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
@@ -11,15 +14,25 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+
 import org.tensorflow.lite.Interpreter;
 
 public class Fragment1 extends Fragment {
 
     private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1888;
-    private static final int REQUEST_IMAGE_CAPTURE = 12345;
-    private
+    private static final int CAMERA_PERMISSION_CODE = 100;
+    private static final String MODEL_PATH = "detect.tflite";
     Button PrendrePhoto;
     ImageView imageView;
 
@@ -38,14 +51,43 @@ public class Fragment1 extends Fragment {
             public void onClick(View view) {
 
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent,
-                        CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+                checkPermission(Manifest.permission.CAMERA, CAMERA_PERMISSION_CODE);
+                startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
 
             }
         });
-
         return rootView;
 
+    }
+
+    public void checkPermission(String permission, int requestCode)
+    {
+        if (ContextCompat.checkSelfPermission(getContext(), permission) == PackageManager.PERMISSION_DENIED) {
+
+            // Requesting the permission
+            ActivityCompat.requestPermissions(getActivity(), new String[] { permission }, requestCode);
+        }
+        else {
+            Toast.makeText(getContext(), "Permission already granted", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults)
+    {
+        super.onRequestPermissionsResult(requestCode,
+                permissions,
+                grantResults);
+
+        if (requestCode == CAMERA_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(getContext(), "Camera Permission Granted", Toast.LENGTH_SHORT) .show();
+            }
+            else {
+                Toast.makeText(getContext(), "Camera Permission Denied", Toast.LENGTH_SHORT) .show();
+            }
+        }
     }
 
     @Override
@@ -68,5 +110,14 @@ public class Fragment1 extends Fragment {
 
             }
         }
+    }
+
+    private MappedByteBuffer loadModelFile(Activity activity)throws IOException{
+        AssetFileDescriptor fileDescriptor = activity.getAssets().openFd(MODEL_PATH);
+        FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor());
+        FileChannel fileChannel = inputStream.getChannel();
+        long startOffset = fileDescriptor.getStartOffset();
+        long declaredLength = fileDescriptor.getDeclaredLength();
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
     }
 }
